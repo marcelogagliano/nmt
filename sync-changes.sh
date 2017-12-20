973d2f4fb03499b526dd42dc52f97a5b4339044a
Marcelo Gagliano <marcelo.gagliano@gmail.com>
2017-12-20 17:07:00 -0200

    Update-synchronization-script

--- sync-changes.sh
+++ sync-changes.sh
@@ -1,7 +1,7 @@
+git add $1
 git status
-git add .
-git commit -m Comment
-git remote add origin https://github.com/marcelogagliano/nmt.git
+git commit -m $2
+#git remote add origin https://github.com/marcelogagliano/nmt.git
+#git remote add origin $3
 git remote -v
 git push origin master
-
